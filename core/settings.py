eee8fb445fd2e7197c044f2c4226eb06fac609bc
elnino799 <alijaefari799@gmail.com>
2026-05-12 19:38:02 +0330

    Add account app and other missing files

--- core/settings.py
+++ core/settings.py
@@ -134,8 +134,7 @@ import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Media files (برای تصاویر محصولات)
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
